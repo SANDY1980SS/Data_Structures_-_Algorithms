64faa1df0d11fc094f76b5b7889f314d19cf0e23
SHADAB AHMED <134635760+SANDY1980SS@users.noreply.github.com>
2024-09-29 19:50:45 +0530

    Update Two_Sum.java

--- Two_Sum.java
+++ Two_Sum.java
@@ -2,17 +2,15 @@ package leetCode;
 
 public class Two_Sum {
     public int[] twoSum(int[] nums, int target) {
-        int[] a = new int[2];
-        for(int i=0;i<nums.length-1;i++){
+        for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++)
                 if(nums[i]+nums[j]==target){
-                    a[0] = i;
-                    a[1] = j;
+                    int[] a = {i,j};
+                    return a;
                 }
         }
-        return a;
+        return null;
     }
-    public static void main(String[] args) {
-
+    public static void main(String[] argc){
+        
     }
-}
