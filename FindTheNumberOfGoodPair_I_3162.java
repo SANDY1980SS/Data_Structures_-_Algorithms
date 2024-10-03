0f77d487ec51f278200e4fcba0267497c497b3f6
SHADAB AHMED <134635760+SANDY1980SS@users.noreply.github.com>
2024-10-03 21:02:47 +0530

    Add files via upload

---
+++ FindTheNumberOfGoodPair_I_3162.java
@@ -0,0 +1,19 @@
+package leetCode;
+
+public class FindTheNumberOfGoodPair_I_3162 {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int pair=0;
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]%(nums2[j]*k)==0){
+                    pair++;
+                }
+            }
+        }
+        return pair;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
