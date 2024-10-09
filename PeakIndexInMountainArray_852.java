3ffce9f99194059d1ab1fd5ddce89ca2dfe7cf76
SHADAB AHMED <134635760+SANDY1980SS@users.noreply.github.com>
2024-10-09 21:01:36 +0530

    Add files via upload

---
+++ PeakIndexInMountainArray_852.java
@@ -0,0 +1,25 @@
+package leetCode;
+
+public class PeakIndexInMountainArray_852 {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 1;
+        int end = arr.length-2;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid] > arr[mid-1]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
